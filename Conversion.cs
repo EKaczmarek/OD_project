beb7a8fa3cc91f868f7a3639b5ea744b2fa17959
EKaczmarek <e.kaczmarek01@gmail.com>
2018-11-03 17:18:35 +0100

    First commit

---
+++ Conversion.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OD_project_console
+{
+    public static class Conversion
+    {
+        private static byte[] asciiCodes;
+
+        // Dlugosc skrótu bloku 32B
+        private static int hash_len = 32;
+
+        // Dlugosc bloku 8B
+        private static int block_len = 8;
+
+        // message
+        private static string message;
+
+        private static string dict_for_hash;
+
+        private static byte[] hash_bytes;
+
+        private static string result_count = "";
+        public static void set_message(string _message)
+        {
+            message = _message;
+        }
+        private static void fulfill()
+        {
+            byte[] temp = Encoding.UTF8.GetBytes(message);
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                asciiCodes[i] = temp[i];
+            }
+
+            //dopelnienie wg ISO 7816-4
+            asciiCodes[message.Length] = 0x80;
+            for (int j = message.Length + 1; j < asciiCodes.Length - 1; j++)
+            {
+                asciiCodes[j] = 0x00;
+            }
+        }
+        private static void xor()
+        {
+            //Console.WriteLine("Wejście asciiCodes:");
+            //for (int i = 0; i < asciiCodes.Length; i++)
+            //    Console.Write(asciiCodes[i] + ",");
+
+            //Console.WriteLine("\nWejście for_hash:");
+            //for (int i = 0; i < dict_for_hash.Length; i++)
+            //    Console.Write(dict_for_hash[i] + ",");
+
+
+            string bajt = "";
+            while (bajt.Length / 8 < hash_len - 1) {
+                for (int j = 0; j < asciiCodes.Length - 2; j += 2)
+                {
+                    for (int k = 0; k < 8; k++)
+                    {
+                        bool test = GetBit(asciiCodes[j], k) ^ (GetBit(asciiCodes[j + 2], k));
+                        if (test) bajt += 1;
+                        else bajt += 0;
+                        if ((bajt.Length) / 8 == hash_len - 1)
+                            break;
+                    }
+                    if (bajt.Length / 8 == hash_len - 1)
+                        break;
+                }
+            }
+            //na wysciu powinny byc 32 B
+            hash_bytes = getBitwiseByteArray(bajt);
+            List<byte> byteArray = hash_bytes.ToList(); //array is the byte array
+            hash_bytes = byteArray.ToArray();
+
+            //testowanie dla róznych dlugosci hashy i blokow
+            //xor slownik dict_for_hash ^ hash_bytes
+            string hash_res = "";
+            bool temp;
+            for (int i = 1; i < hash_len + 1; i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    if (dict_for_hash[i%j] == '0')
+                        temp = false;
+                    else temp = true;
+                    bool test = GetBit(hash_bytes[i%j], j-1) ^ temp;
+
+                    if (test) hash_res += "1";
+                    else hash_res += "0";
+                }
+            }
+            //na wysciu powinny byc hash_len dlugosc B
+            hash_bytes = getBitwiseByteArray(hash_res);
+            List<byte> byteArray2 = hash_bytes.ToList(); //array is the byte array
+            hash_bytes = byteArray2.ToArray();
+
+
+            //xor hash_bytes ^ numer pod jakim są w tablicy
+            string test_hash = "";
+            for(int i = 1; i < hash_len + 1; i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    bool test = GetBit(hash_bytes[i-1], j) ^ GetBit((byte)(i-1), j);
+                    if (test) test_hash += "1";
+                    else test_hash += "0";
+
+                }
+            }
+
+            hash_bytes = getBitwiseByteArray(test_hash);
+            List<byte> byteArray3 = hash_bytes.ToList(); //array is the byte array
+            hash_bytes = byteArray3.ToArray();
+
+            //Console.WriteLine("\nWynikowe bajty:");
+            string result_hash = "";
+            for (int j = 1; j < hash_len + 1; j++)
+            {
+                int a = hash_bytes[j-1] % (dict_for_hash.Length - 1);
+                //Console.Write(a + ", ");
+                result_hash += dict_for_hash[a];
+            }
+            //Console.WriteLine("Wynikowy hash: " + result_hash);
+            //Console.WriteLine("Długość hasha: " + result_hash.Length);
+
+            result_count = result_hash;
+        }
+
+        private static void xor_dictionary_hasbytes() {
+        }
+        
+        private static byte[] getBitwiseByteArray(string input)
+        {
+            List<byte> byteList = new List<byte>();
+            for (int i = input.Length - 1; i >= 0; i -= 8)
+            {
+                string byteString = "";
+                for (int j = 0; (i - j) >= 0 && j < 8; j++)
+                {
+                    byteString = input[i - j] + byteString;
+                }
+                if (byteString != "")
+                {
+                    byteList.Add(Convert.ToByte(byteString, 2));
+                }
+            }
+            return byteList.ToArray();
+        }
+
+        public static bool GetBit(this byte b, int bitNumber)
+        {
+            return (b & (1 << bitNumber)) != 0;
+        }
+
+
+        public static void chars_for_hash()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            //NUMBERS
+            for (int i = 48; i < 58; i++)
+            {
+                char symbol = (char)i;
+                //Console.WriteLine("{0} -> {1}", i, symbol);
+                dict_for_hash += symbol;
+            }
+            //LETTERS
+            for (int i = 97; i < 123; i++)
+            {
+                char symbol = (char)i;
+                //Console.WriteLine("{0} -> {1}", i, symbol);
+                dict_for_hash += symbol;
+            }
+            //Console.WriteLine("Rozmiar tablicy: " + dict_for_hash.Length.ToString());
+        }
+        public static string count()
+        {
+            chars_for_hash();
+            asciiCodes = new byte[((message.Length / 8) + 1) * 8];
+            hash_bytes = new byte[32];
+
+            //Console.Write("Message: ", message);           
+
+            //for (int i = 0; i < asciiCodes.Length; i++)
+            //{
+            //    Console.Write(i.ToString() + " " + asciiCodes[i].ToString() + "\n");
+            //}
+
+            fulfill();
+            xor();
+            
+           
+            return result_count;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OD_project_console
+{
+    class Program
+    {
+        public static string RandomString(int size, bool lowerCase)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            if (lowerCase)
+                return builder.ToString().ToLower();
+            return builder.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            string text;
+            int words_ammout = 1000;
+            int words_lenght = 12;
+
+            List<string> words = new List<string>();
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            while (words.Count != words_ammout)
+            {
+                text = RandomString(words_lenght, true);
+                if (words.Count == 0)
+                {
+                    words.Add(text);
+                }
+                else
+                {
+                    if (!words.Contains(text))
+                        words.Add(text);
+                }
+                //System.Console.WriteLine("\nWiadomość: " + text);
+
+                //string[] separators = new string[] { ",", ".", "!", "\'", " ", "\'s" };
+
+                //foreach (string word in text.Split(separators, StringSplitOptions.RemoveEmptyEntries))
+                //{
+                //    if (!words.Contains(word))
+                //    {
+                //        words.Add(word);
+                //        //Console.WriteLine(word);
+                //    }
+                //}
+            }
+
+
+            foreach (string item in words) { 
+
+                Conversion.set_message(item);
+                string result = Conversion.count();
+                dictionary.Add(item, result);
+            }
+
+            int hash_lenght = dictionary[words[0]].Length;
+            //foreach (KeyValuePair<string, string> item in dictionary)
+            //{
+            //    Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+            //}
+
+
+            int kolizje = 0;
+            var lookup = dictionary.ToLookup(x => x.Value).Where(x => x.Count() > 1);
+            foreach (var item in lookup)
+            {
+                var keys = item.Aggregate("", (s, v) => s + ", " + v);
+                var message = "te same wartości maja: " + item.Key + ":" + keys;
+                Console.WriteLine(message);
+                kolizje++;
+            }
+            Console.WriteLine("\nDlugosc stringow wejsciowych: " + words_lenght);            
+            Console.WriteLine("Ilość stringów wejściowych: " + words_ammout);
+            Console.WriteLine("Długość hashy: " + hash_lenght);
+            Console.WriteLine("Ilość kolizji: " + kolizje);
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
